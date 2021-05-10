5291d5cc06f2489083bc730e6b568bb15166b316
Julli4n <11952329+Julli4n@users.noreply.github.com>
2021-05-10 01:46:41 -0700

    do sass stuff

--- src/pages/Module/Settings/data.lua
+++ src/pages/Module/Settings/data.lua
@@ -1,8 +1,3 @@
---[=[ 
-- Module:Settings/data from the Bubble Gum Simulator Wiki on Fandom. https://bubble-gum-simulator.fandom.com
-- Redistribution and reference is permitted as long as proper attribution is given (per CC BY-SA), but direct re-usage is not recommended as this Lua code was and is created specifically for the Bubble Gum Simulator Wiki
-- See https://www.fandom.com/licensing for more information about licensing
-]=]
 local function LoadModule(name, other)
 	local Module
 	pcall(function() Module = other and require(name) or mw.loadData(name) end)
