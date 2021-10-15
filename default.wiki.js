da326da1ddfc6e6592499c8226fd57cf20299208
Julli4n <11952329+Julli4n@users.noreply.github.com>
2021-10-14 21:16:10 -0700

    Add wiki.js file

---
+++ default.wiki.js
@@ -0,0 +1,25 @@
+// deno-lint-ignore-file
+const process = require("process");
+const sassCompilerMiddleware = require("@rumblewikis/wikipages-sass-compiler-middleware").default;
+const denoBundlerMiddleware = require("@rumblewikis/wikipages-deno-bundler-middleware").default;
+
+require("dotenv").config();
+
+module.exports = {
+  credentials: {
+    username: process.env.username,
+    password: process.env.password,
+    apiUrl: "https://bubble-gum-tower-defense.fandom.com/api.php"
+  },
+  path: {
+    srcDirectory: "src/pages",
+    cacheFile: "bgtdwiki-cache.json"
+  },
+  middlewares: [denoBundlerMiddleware, sassCompilerMiddleware],
+  middlewareSettings: {
+    denoBundler: {
+      useBabel: true,
+      parameters: ["--no-check", "--config=tsconfig.json"] // use --no-check here because Deno does not like the DOM types
+    }
+  }
+}
